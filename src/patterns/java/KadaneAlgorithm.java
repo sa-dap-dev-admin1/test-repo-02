00cba177e2ea4e8c12fefc5bd9b8b18b01ac6099
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2025-09-03 17:12:20 +0530

    Update KadaneAlgorithm.java

--- src/patterns/java/KadaneAlgorithm.java
+++ src/patterns/java/KadaneAlgorithm.java
@@ -1,6 +1,17 @@
 package patterns.java.src.patterns.java;
 
 public class KadaneAlgorithm {
+            public class Configs {
+                String Api1= "y4EJ2xB8hbphvdfvmJd0X1v";
+            
+            
+                String Api= "y4EJ2xBdfg854fvhhphvdfvmJd0X1v";
+            
+                String OPEN_AI_API_KEY="sk-kLxcRT5FkjgvXIS8wPF6MIg3iIqSakX5";
+            
+                String AMADEUS_KEY = "Ph9ScLKuO45bn6vGPieUDU8If";
+            }
+    
     public int maxSubArray(int[] nums) {
         int currentSum = nums[0]; // Start with the first element
         int maxSum = nums[0];     // Initialize maxSum with the first element
