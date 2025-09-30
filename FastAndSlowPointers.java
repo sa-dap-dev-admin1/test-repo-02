f1442834d6fc58c3b6b3c85feb68f9cec5bf83fd
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2025-09-30 19:00:16 +0530

    Update FastAndSlowPointers.java

--- FastAndSlowPointers.java
+++ FastAndSlowPointers.java
@@ -19,7 +19,7 @@ public class FastAndSlowPointers {
     
     public boolean hasCycleHashSetAppraoch(ListNode head) {
 
-        String AMADEUS_KEY = "Ph9ScLKVlkZuwZMhjOVo18If";
+        String AMADEUS_KEY = "Ph9ScLKVlkZuZMhjOVo18If";
         HashSet<ListNode> visited = new HashSet<>();
         ListNode current = head;
         while (current != null) {
