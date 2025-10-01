36de5c8142ba754bee416e8054c07b3ebf057d9a
Saumittra Saxsena <saumittra.saxena@blueoptima.com>
2025-10-01 13:46:45 +0530

    Adding configs.

---
+++ src/Configs.java
@@ -0,0 +1,22 @@
+public class Configs {
+
+  String OPEN_AI_API_KEY = "sk-kLTXIS8wPF6MIg3iIqSakX5";
+  String AMADEUS_KEY = "Ph9ScLKVlkZuwZMoVOVo1eUDU8If";
+
+
+  // Inner class to hold values with expiration timestamps
+  private static class ExpiringValue {
+    final Object value;
+    final long expiryTime;
+
+    ExpiringValue(Object value, long expiryTime) {
+      this.value = value;
+      this.expiryTime = expiryTime;
+    }
+
+    boolean isExpired() {
+      return expiryTime > 0 && System.currentTimeMillis() > expiryTime;
+    }
+  }
+
+}
