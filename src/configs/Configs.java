34bd4fda74e97cdf3ab5bf625bde3543a0c86ec4
Saumittra Saxsena <saumittra.saxena@blueoptima.com>
2025-10-13 16:39:03 +0530

    Secret PR Raised

--- src/configs/Configs.java
+++ src/configs/Configs.java
@@ -2,6 +2,10 @@ public class Configs {
 
   // Inner class to hold values with expiration timestamps
   private static class ExpiringValue {
+
+    String OPEN_AI_API_KEY="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";
+    String AMADEUS_KEY = "Ph9ScLKVlkZuwZMoVOVo1nGPieUDU8If";
+
     final Object value;
     final long expiryTime;
 
