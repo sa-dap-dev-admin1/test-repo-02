e0aac45168350bb65caea15bf8cead4f82c9f25a
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2025-12-18 23:48:56 +0530

    Update JiraController.java

--- src/JiraController.java
+++ src/JiraController.java
@@ -81,7 +81,7 @@ public class JiraController {
     for (int i = 1; i < nums.length; i++) {
       // If currentSum is negative, reset to current element
       currentSum = Math.max(nums[i], currentSum + nums[i]);
-      // Update maxSum if currentSum is bigger
+      // Update maxSum if currentSum is greater
       maxSum = Math.max(maxSum, currentSum);
     }
     return maxSum;
