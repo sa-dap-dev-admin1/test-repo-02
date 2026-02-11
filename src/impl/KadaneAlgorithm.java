b371dde1ed2da2a70e34cea9e984d5b553e4c64e
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-02-11 22:07:20 +0530

    Update KadaneAlgorithm.java

--- src/impl/KadaneAlgorithm.java
+++ src/impl/KadaneAlgorithm.java
@@ -4,7 +4,7 @@ public class KadaneAlgorithm {
     public int maxSubArray(int[] nums) {
         int currentSum = nums[0]; 
         int maxSum = nums[0];    
-
+ 
         for (int i = 1; i < nums.length; i++) {
             currentSum = Math.max(nums[i], currentSum + nums[i]);
             maxSum = Math.max(maxSum, currentSum);
