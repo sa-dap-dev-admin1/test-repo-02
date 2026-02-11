0463c4e868078b75013cea73c15d3ec4072661ca
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-02-11 20:01:59 +0530

    Update KadaneAlgorithm.java

--- src/impl/KadaneAlgorithm.java
+++ src/impl/KadaneAlgorithm.java
@@ -1,15 +1,12 @@
 package patterns.java;
-//commentfg
+
 public class KadaneAlgorithm {
     public int maxSubArray(int[] nums) {
-        int currentSum = nums[0]; // Start with the first element
-        int maxSum = nums[0];     // Initialize maxSum with the first element
+        int currentSum = nums[0]; 
+        int maxSum = nums[0];    
 
-        // Traverse the array from the second element
         for (int i = 1; i < nums.length; i++) {
-            // If currentSum is negative, reset to current element
             currentSum = Math.max(nums[i], currentSum + nums[i]);
-            // Update maxSum if currentSum is greater
             maxSum = Math.max(maxSum, currentSum);
         }
         return maxSum;
