7c655b1fa066221edf1cf5708ccb4a256cae7d87
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-02-12 00:56:09 +0530

    Update JiraController.java

--- src/impl/JiraController.java
+++ src/impl/JiraController.java
@@ -67,7 +67,7 @@ public class JiraController {
           logger.error("Error in file reading: ",e);
           throw e;
       }
-         // Testing UAT : 20
+         // Testing UAT : 21
 
       return message;
 
