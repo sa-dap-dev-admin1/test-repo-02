db46023dcd006b88b00d7a565cb1bf7960a46ad1
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-02-16 16:40:39 +0530

    Update 1.java
    s

--- src/impl/1.java
+++ src/impl/1.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int a = 109;
-        int b = 25;
+        int b = 255;
 
         int sum = a + b;
         System.out.println("Sum = " + sum);
