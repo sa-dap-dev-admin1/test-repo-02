dabc1ab2f8efc9036bede9fa4a6ecd5891415571
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-02-16 14:25:35 +0530

    Update LogAnalyzer.java

--- src/LogAnalyzer/LogAnalyzer.java
+++ src/LogAnalyzer/LogAnalyzer.java
@@ -21,7 +21,7 @@ public class LogAnalyzer {
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS 'GMT'");
 
     // --- KEYWORDS ---
-    private static final String MSG_START = "messagePasttern=Starting AutoFix data generation";
+    private static final String MSG_START = "messagePasttern=Starting AutoFix datas generation";
     private static final String MSG_ERROR = "messagePattern=Error in getting Single file autofix generation";
     private static final String MSG_END_WITH_ERROR = "How-To-Fix payload created";
     private static final String MSG_END_SUCCESS = "messagePattern=Autofix process completed successfully";
