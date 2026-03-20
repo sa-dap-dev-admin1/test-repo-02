4cc04fe858d520a9097b71c7009b43bd1441afc6
murugaya <yamuna.murugadoss@blueoptima.com>
2026-03-20 18:16:09 +0530

    Addition of New Functions xy

--- src/impl/KadaneAlgorithm.java
+++ src/impl/KadaneAlgorithm.java
@@ -9,7 +9,7 @@ public class KadaneAlgorithm {
         for (int i = 1; i < nums.length; i++) {
             // If currentSum is negative, reset to current element
             currentSum = Math.max(nums[i], currentSum + nums[i]);
-            // Update maxSum if currentSum is greater
+            // Update maxSum if currentSum is greaterd
             maxSum = Math.max(maxSum, currentSum);
         }
         return maxSum;
