c954057d81a94cfab96d6c1eeb345fa6b3da33c2
sa-dap-dev-admin1 <80357408+sa-dap-dev-admin1@users.noreply.github.com>
2026-06-10 17:13:24 +0530

    Update SCR.java

--- src/impl/SCR.java
+++ src/impl/SCR.java
@@ -123,7 +123,7 @@ public class SCR extends BOpInfraExtractor {
 	private final RevisionRouter revisionRouter;
 	private final SDFCService sdfcService;
 
-	// Testing Batching _07
+	// Testing Batching _08
 
 	private BOpSCRData scrData = new BOpSCRData();
 	private String publisherResponse;
